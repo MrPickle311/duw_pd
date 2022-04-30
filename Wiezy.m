bfefc3c2100ba23cedae32e2677de64db0ee8594
MrPickle311 <macmacroll@gmail.com>
2022-04-30 21:10:49 +0200

    Finished wiezy

--- Wiezy.m
+++ Wiezy.m
@@ -1,7 +1,5 @@
-function F=Wiezy(q,t)
+function F=Wiezy(q,t,rot_pairs,bodies,body0)
 % Przypisanie elementom wektora q czytelnych nazw
-
-
 
 r0=[0;0];       fi0 = 0;
 r1=q(1:2);      fi1=q(3);    
@@ -18,24 +16,60 @@ r10=q(28:29);   fi10=q(30);
 % Rownania wiezow
 
 % pary obrotowe
-F(1:2,1)= rotate_pair(r1,fi1,s1A_1,r4,fi4,s4A_4); % para 1-4 punkt A
-F(3:4,1)= rotate_pair(r4,fi4,s4B_4,r5,fi5,s5B_5); % para 4-5 punkt B
-F(5:6,1)= rotate_pair(r3,fi3,s3C_3,r5,fi5,s5C_5); % para 3-5 punkt C
-F(7:8,1)= rotate_pair(r1,fi1,s1D_1,r10,fi10,s10D_10); % para 1-10 punkt D
-F(9:10,1)= rotate_pair(r3,fi3,s3E_3,r10,fi10,s10E_10); % para 3-10 punkt E
-F(11:12,1)= rotate_pair(r2,fi2,s2F_2,r3,fi3,s3F_3); % para 2-3 punkt F
-F(13:14,1)= rotate_pair(r2,fi2,s2G_2,r9,fi9,s9G_9); % para 2-9 punkt G
-F(15:16,1)= rotate_pair(r0,fi0,s0H_0,r8,fi8,s8H_8); % para 0-8 punkt H
-F(17:18,1)= rotate_pair(r7,fi7,s7M_7,r10,fi10,s10M_10); % para 7-10 punkt M
-F(19:20,1)= rotate_pair(r0,fi0,s0N_0,r6,fi6,s6N_6); % para 0-6 punkt N
-F(21:22,1)= rotate_pair(r0,fi0,s0O_0,r2,fi2,s2O_2); % para 0-2 punkt O
-F(23:24,1)= rotate_pair(r1,fi1,s1P_1,r2,fi2,s2P_2); % para 1-2 punkt P
+
+rot_size = size(rot_pairs);
+
+for n = 1:rot_size
+    i = rot_pairs(n).body_i;
+    j = rot_pairs(n).body_j;
+    point = rot_pairs(n).point;
+    
+    if i == 0
+        ri = 0;
+        fi_i = fi0;
+        s_i = get_local_vector_from_body_0(body0,point);
+    else
+        ri = q(3*i-2 : 3*i-1);
+        fi_i = q(3*i);
+        s_i = get_local_vector(bodies,i,point);
+    end
+    
+    rj = q(3*j-2 : 3*j-1);
+    fi_j = q(3*j);
+    s_j = get_local_vector(bodies,j,point);
+
+    F(2*n-1:2*n,1) = rotate_pair(ri,fi_i,s_i,rj,fi_j,s_j);
+end
+
+% F(1:2,1)= rotate_pair(r1,fi1,s1A_1,r4,fi4,s4A_4); % para 1-4 punkt A
+% F(3:4,1)= rotate_pair(r4,fi4,s4B_4,r5,fi5,s5B_5); % para 4-5 punkt B
+% F(5:6,1)= rotate_pair(r3,fi3,s3C_3,r5,fi5,s5C_5); % para 3-5 punkt C
+% F(7:8,1)= rotate_pair(r1,fi1,s1D_1,r10,fi10,s10D_10); % para 1-10 punkt D
+% F(9:10,1)= rotate_pair(r3,fi3,s3E_3,r10,fi10,s10E_10); % para 3-10 punkt E
+% F(11:12,1)= rotate_pair(r2,fi2,s2F_2,r3,fi3,s3F_3); % para 2-3 punkt F
+% F(13:14,1)= rotate_pair(r2,fi2,s2G_2,r9,fi9,s9G_9); % para 2-9 punkt G
+% F(15:16,1)= rotate_pair(r0,fi0,s0H_0,r8,fi8,s8H_8); % para 0-8 punkt H
+% F(17:18,1)= rotate_pair(r7,fi7,s7M_7,r10,fi10,s10M_10); % para 7-10 punkt M
+% F(19:20,1)= rotate_pair(r0,fi0,s0N_0,r6,fi6,s6N_6); % para 0-6 punkt N
+% F(21:22,1)= rotate_pair(r0,fi0,s0O_0,r2,fi2,s2O_2); % para 0-2 punkt O
+% F(23:24,1)= rotate_pair(r1,fi1,s1P_1,r2,fi2,s2P_2); % para 1-2 punkt P
+
+% para 6-7
+fi_6_7 = -pi; % staly kat obrotu ukladu 6 wzgledem 7
+dNM7 = [1 0]'; % wersor ruchu wzglêdnego w ukladzie 6
+vNM7 = [0 1]'; % wersor prostopad³y do osi ruchu wzglednego w ukladzie 6
+% Para 8-9
+fi_8_9 = -pi; % staly kat obrotu ukladu 8 wzgledem 9
+dHG8 = [1 0]'; % wersor ruchu wzglêdnego w ukladzie 8
+vHG8 = [0 1]'; % wersor prostopadly do osi ruchu wzglednego w ukladzie 8
 
 % para postepowa 6-7 , NM
-F(25:26,1) = progressive_pair(r6,fi6,s6N_6,r7,fi7,s7M_7,fi_6_7,vNM7);
+F(25:26,1) = progressive_pair(r6,fi6,bodies{6}{1}.local_vec,...
+                              r7,fi7,bodies{7}{1}.local_vec,fi_6_7,vNM7);
 
 % para postepowa 8-9 , HG
-F(27:28,1) = progressive_pair(r8,fi8,s8H_8,r9,fi9,s9G_9,fi_8_9,vHG8);
+F(27:28,1) = progressive_pair(r8,fi8,bodies{8}{1}.local_vec,...
+                              r9,fi9,bodies{9}{1}.local_vec,fi_8_9,vHG8);
 
 % wiezy kierujace
 
@@ -46,5 +80,7 @@ fi_k = pi;
 
 func = @(t_i) lk + ak*sin(wk*t_i + fi_k);
 
-F(29,1) = relative_displacement_in_progressive_pair(r6,fi6,s6N_6,r7,fi7,s7M_7,dNM7,func,t);
-F(30,1) = relative_displacement_in_progressive_pair(r8,fi8,s8H_8,r9,fi9,s9G_9,dHG8,func,t);
+F(29,1) = relative_displacement_in_progressive_pair(r6,fi6,bodies{6}{1}.local_vec,...
+                                                    r7,fi7,bodies{7}{1}.local_vec,dNM7,func,t);
+F(30,1) = relative_displacement_in_progressive_pair(r8,fi8,bodies{8}{1}.local_vec,...
+                                                    r9,fi9,bodies{9}{1}.local_vec,dHG8,func,t);
