930fbccc79921921346b628eb602f8e6b158f9a7
MrPickle311 <macmacroll@gmail.com>
2022-04-30 21:16:15 +0200

    First working version of script, but not convergent yet

--- NewRaph.m
+++ NewRaph.m
@@ -1,21 +1,10 @@
-function q=NewRaph(q0,t)
-% q=NewRaph(q0,t)
-%   Rozwi¹zanie uk³adu równañ metod¹ Newtona-Raphsona.
-% Wejœcie:
-%   q0 - przybli¿enie startowe,
-%   t - chwila, dla której poszukiwane jest rozwi¹zanie.
-% Wyjœcie:
-%   q - uzyskane rozwi¹zanie.
-%
-% Uk³ad równañ musi byæ wpisany w pliku Wiezy.m.
-% Macierz Jacobiego uk³adu równañ musi byæ wpisana w pliku Jakobian.m.
-
+function q=NewRaph(q0,t,rot_pairs,bodies,body0)
 q=q0;
-F=Wiezy(q,t);
+F=Wiezy(q,t,rot_pairs,bodies,body0);
 iter=1; % Licznik iteracji
-while ( (norm(F)>1e-10) & (iter < 25) )
-    F=Wiezy(q,t);
-    Fq=Jakobian(q);
+while ( (norm(F)>1e-10) && (iter < 25) )
+    F=Wiezy(q,t,rot_pairs,bodies,body0);
+    Fq=Jakobian(q,rot_pairs,bodies,body0);
     q=q-Fq\F;  % Fq\F jest równowa¿ne inv(Fq)*F, ale mniej kosztowne numerycznie
     iter=iter+1;
 end
--- main.m
+++ main.m
@@ -1,5 +1,28 @@
-clear
-clc
-[rot_pairs,body_0,bodies] = moje_dane();
-q0 = zeros(30,1);
-result=Jakobian(q0,rot_pairs,bodies,body_0);
+function [T,Q] = main()
+    clear
+    clc
+    size = 30;
+    q=zeros(size,1);
+    lroz=0; % Licznik rozwiązań (służy do numerowania kolumn w tablicach z wynikami)
+    dt=0.05; % Odstęp pomiędzy kolejnymi chwilami
+    
+    
+    % wczytaj dane dla ukladu
+    [rot_pairs,body0,bodies] = moje_dane();
+    
+    q=zeros(size,1);
+    q(1) = 5;
+    dq=zeros(size,1); 
+    d2q=zeros(size,1);
+    t = 1;
+%     result=Jakobian(q0,rot_pairs,bodies,body_0);
+%     F=Wiezy(q0,t,rot_pairs,bodies,body_0);
+    
+    for t=0:dt:4.0
+        q0=q+dq*dt+0.5*d2q*dt^2;
+        q=NewRaph(q0,t,rot_pairs,bodies,body0);
+        lroz=lroz+1;
+        T(1,lroz)=t; 
+        Q(:,lroz)=q;
+    end
+end
