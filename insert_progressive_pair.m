e50ac4225bf6840356b4c01b81d16fb20d543f20
MrPickle311 <macmacroll@gmail.com>
2022-05-13 02:14:59 +0200

    Improved constrain pairs

--- insert_progressive_pair.m
+++ insert_progressive_pair.m
@@ -1,11 +1,12 @@
-function [result] = insert_rotation_pair_into_jacobi(jacobi_matrix,body_i,body_j,row,fi_i,si,fi_j,sj)
+function [result] = insert_progressive_pair(jacobi_matrix,body_i,body_j,row,ri,fi_i,si,rj,fi_j,vec)
     % niestety matlab przekazuje tylko przez wartosc, pass by value
     first_row = 2*row - 1;
     second_row = 2*row;
-    temp = jacobi_element_for_rotation_pair(fi_i,si,fi_j,sj);
+    temp = jacobi_element_for_progressive_pair(ri,fi_i,si,rj,fi_j,vec);
     result = jacobi_matrix;
     if body_i ~= 0
         result(first_row:second_row , 3*body_i - 2 : 3*body_i) = the_first_half_of_constrain_pair(temp);
     end
     result(first_row:second_row , 3*body_j - 2 : 3*body_j) = the_second_half_of_constrain_pair(temp);
 end
+
--- the_first_half_of_rotation_pair.m
+++
@@ -1,4 +0,0 @@
-function result = the_first_half_of_rotation_pair(rotation_pair)
-    result = rotation_pair(1:2,1:3);
-end
-
